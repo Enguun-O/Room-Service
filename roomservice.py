5718d3856ef0306f5e9084d02178107faa8503c9
Enguun-O <98449387+Enguun-O@users.noreply.github.com>
2022-03-03 14:53:32 +0800

    Create roomservice.py

---
+++ roomservice.py
@@ -0,0 +1,14 @@
+guests = {'1':'Jonathan', '2': 'Joseph', '3': 'Jotaro'}
+correct_info = False
+room = input ('Pleae, enter your room number: ')
+if room in guests.keys():
+    correct_info = True
+else:
+    correct_info = False
+name = input ('Please, enter your name: ') 
+if correct_info and guests[room].lower()==name.lower():
+    correct_info = True
+    print('Correct')
+else:
+    correct_info = False
+    print('Incorrect')
