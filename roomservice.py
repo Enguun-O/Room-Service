c78f00d4c80f17a3ecaa993b4e715a7b81976c28
Enguun-O <98449387+Enguun-O@users.noreply.github.com>
2022-03-03 15:04:42 +0800

    while and startmeal start

--- roomservice.py
+++ roomservice.py
@@ -5,15 +5,19 @@ Made by ...
 
 guests = {'1':'Jonathan', '2': 'Joseph', '3': 'Jotaro'}
 correct_info = False
-room = input ('Pleae, enter your room number: ')
-if room in guests.keys():
-    correct_info = True
-else:
-    correct_info = False
-name = input ('Please, enter your name: ') 
-if correct_info and guests[room].lower()==name.lower():
-    correct_info = True
-    print('Correct')
-else:
-    correct_info = False
-    print('Incorrect')
+room = None
+name = None
+while not correct_info:
+    room = input ('Pleae, enter your room number: ')
+    if room in guests.keys():
+        correct_info = True
+    else:
+        correct_info = False
+    name = input ('Please, enter your name: ') 
+    if correct_info and guests[room].lower()==name.lower():
+        correct_info = True
+        print('Correct')
+    else:
+        correct_info = False
+        print('Incorrect')
+    startmeal = input ('What would you like for your starter meal?')
